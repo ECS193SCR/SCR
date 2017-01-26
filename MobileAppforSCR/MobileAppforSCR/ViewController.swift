9e92e45afafa987ffe96f436a89bf79cf3bb2089
ckloi <ckloi@ucdavis.edu>
2017-01-26 13:36:34 -0800

    add_sdk

--- MobileAppforSCR/MobileAppforSCR/ViewController.swift
+++ MobileAppforSCR/MobileAppforSCR/ViewController.swift
@@ -1,25 +1,70 @@
 //
 //  ViewController.swift
-//  MobileAppforSCR
+//  SCR
 //
-//  Created by Yixin Hu on 24/01/2017.
-//  Copyright © 2017 Heritage Chinese Center. All rights reserved.
+//  Created by Chi Kei on 1/25/17.
+//  Copyright © 2017 HeritageChineseCenter. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+
+
+class ViewController: UIViewController, IFlySpeechEvaluatorDelegate {
+    
+    
+    var evaluator = IFlySpeechEvaluator.sharedInstance()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        evaluator?.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
+//    - (void)onVolumeChanged:(int)volume buffer:(NSData *)buffer{} //
+//    - (void)onBeginOfSpeech{}
+//    //
+//    - (void)onEndOfSpeech{}
+//    //
+//    - (void)onCancel{}
+//    //
+//    - (void)onError:(IFlySpeechError *)errorCode{}
+//    //
+//    - (void)onResults:(NSData *)results isLast:(BOOL)isLast{}
+    
+    func onBeginOfSpeech() {
+        
+    }
+    
+    func onEndOfSpeech() {
+        
+    }
+    
+    func onCancel() {
+        
+    }
+    
+    func onError(_ errorCode: IFlySpeechError!) {
+        
+    }
+    func onResults(_ results: Data!, isLast: Bool) {
+        
+    }
+    
+    func onVolumeChanged(_ volume: Int32, buffer: Data!) {
+        
+    }
+    
+    
 
 }
 
---
+++ MobileAppforSCR/MobileAppforSCR/bridging_header.h
@@ -0,0 +1,15 @@
+//
+//  bridging_header.h
+//  MobileAppforSCR
+//
+//  Created by Chi Kei on 1/26/17.
+//  Copyright © 2017 Heritage Chinese Center. All rights reserved.
+//
+
+#ifndef bridging_header_h
+#define bridging_header_h
+
+#import "iflyMSC/IFlyMSC.h"
+
+
+#endif /* bridging_header_h */
